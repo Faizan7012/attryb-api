ca2a8f14d1f52c92b1c53bd7902030784ae1967f
Faizan7012 <frtoons7012@gmail.com>
2023-05-27 08:06:39 +0530

    new

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const connection = require('../config/db');
 const userRoute = require('../routes/user.routes');
 const carRoute = require('../routes/cars.routes');
+const formModel = require('../model/form.model');
 
 const app = express();
 
@@ -17,6 +18,24 @@ app.use('/car',carRoute)
 
 app.get('/', (req , res)=>{
     res.send('Welcome in Attryb Api')
+})
+
+app.post('/form' , async(req , res)=>{
+    try {
+         let form = await formModel.create({...req.body});
+         res.send({status:true , message : 'form submitted'})
+    } catch (error) {
+        res.send({status:false , message : error.message})
+    }
+})
+
+app.get('/form' , async(req , res)=>{
+    try {
+         let forms = await formModel.find();
+         res.send({status:true , data : forms});
+    } catch (error) {
+        res.send({status:false , message : error.message})
+    }
 })
 
 
